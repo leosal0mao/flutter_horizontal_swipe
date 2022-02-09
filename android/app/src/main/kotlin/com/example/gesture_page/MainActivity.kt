b4a0b3076a7c42bc76f5678236e05dd2445b81ab
Leo Salomao <leonardo.salomao@crmall.com>
2022-02-09 18:05:00 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/gesture_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gesture_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page_two.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: GestureDetector(),
+        title: Text('page 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'Page two',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
